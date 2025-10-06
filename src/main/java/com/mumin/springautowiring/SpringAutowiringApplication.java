c57f6dbde4835d38cbe3510076feb0f62d0c5afd
mumin-pervez <pervez.ist@gmail.com>
2025-10-06 20:58:07 +0600

    Project Initailization

---
+++ src/main/java/com/mumin/springautowiring/SpringAutowiringApplication.java
@@ -0,0 +1,13 @@
+package com.mumin.springautowiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutowiringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAutowiringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mumin/springautowiring/SpringAutowiringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumin.springautowiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAutowiringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
